2fb8b8260f709e8138f226f6e76a5bd1fccad0f1
zxh <zhangxinghai79@gmail.com>
2019-09-08 23:30:27 +0800

    update

---
+++ start/main.js
@@ -0,0 +1,11 @@
+import sub from "./sub"
+import fs from "fs"
+
+console.log("hahah", typeof fs.rename)
+
+sub()
+
+module.exports = {
+    name: "Singhi",
+    age: 30
+}
---
+++ start/sub.js
@@ -0,0 +1,5 @@
+const sub = () => {
+    console.log("hahhaa")
+}
+
+export default sub
